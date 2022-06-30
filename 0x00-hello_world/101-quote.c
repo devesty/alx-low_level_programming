42d4f4b38e4878d02201ac718c1056e037f48c17
devesty <estherharriet2@gmail.com>
2022-06-30 13:35:42 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
   * main - Entry point
   * Return: Always 1 (Success)
@@ -7,6 +7,6 @@
 int main(void)
 {
 writeLine(STDOUT_FILENO, "and that piece of art is useful
-\" - Dora Korpar, 2015-10-19", 59);
+\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
