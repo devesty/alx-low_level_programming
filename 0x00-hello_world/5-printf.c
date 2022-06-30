6043562e502992bda38e9a1bc083116530fc1de8
devesty <estherharriet2@gmail.com>
2022-06-30 12:08:21 -0700

    Printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *  main - Print a string
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+      printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+      return (0);
+}
