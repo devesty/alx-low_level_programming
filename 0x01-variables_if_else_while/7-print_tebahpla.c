caced3d6c94fd41e4bc09f4cfb1d6d47d314e838
devesty <estherharriet2@gmail.com>
2022-07-01 15:25:12 -0700

    Print tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int a = 122;
+
+	while (a >= 97)
+	{
+	putchar(a);
+	a--;
+	}
+	putchar(10);
+	return (0);
+}
