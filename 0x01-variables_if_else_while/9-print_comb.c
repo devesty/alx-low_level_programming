df8af56cc1a936743e70f7bf09451a7d3c92c929
devesty <estherharriet2@gmail.com>
2022-07-01 15:59:45 -0700

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+	putchar(c);
+	if (!(c == 57))
+	{
+	putchar(44);
+	putchar(32);
+	}
+	c++;
+	}
+	putchar(10);
+	return (0);
+}
