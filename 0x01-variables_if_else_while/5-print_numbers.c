4ceea0b312d444e211a3471ede4c0d12292d2cad
devesty <estherharriet2@gmail.com>
2022-07-01 14:35:35 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a = 50;
+
+	while (a <= 70)
+	{
+	putchar(a);
+	a++;
+	}
+	putchar(10);
+	return (0);
+}
