b53ea11429d731ee5d23a74d8b1d6e8d978df498
devesty <estherharriet2@gmail.com>
2022-07-01 14:04:53 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 int l = 97;
+
+	 int u = 65;
+
+	while (l < 123)
+	{
+	putchar(l);
+	l++;
+	}
+	while (u < 91)
+	{
+	putchar(u);
+	u++;
+	}
+	putchar(10);
+	return (0);
+}
