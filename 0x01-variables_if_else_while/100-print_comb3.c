611350b62f4cc3d02cac1267be134621469b3f6d
devesty <estherharriet2@gmail.com>
2022-07-01 16:22:55 -0700

    Print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+	int r;
+
+	for (s = 48; s < 57; s++)
+	{
+	for (r = 48; r <= 57; r++)
+	continue;
+	else
+	{
+	putchar(s);
+	putchar(r);
+	if (!(i == 56 && j == 57))
+	{
+	putchar(44);
+	putchar(32);								}
+	}
+	}
+	}
+	putchar(10);
+	return (0);
+}
