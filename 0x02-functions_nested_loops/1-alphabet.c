d2b461918d039efe1f32874a51153d136e8f12b2
devesty <estherharriet2@gmail.com>
2022-07-05 07:06:18 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar (letter);
+	}
+	_putchar (10);
+
+}
