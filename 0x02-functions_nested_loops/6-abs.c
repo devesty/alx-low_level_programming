8e5c21a43741562620bd774f7aa600e4f6f0158b
devesty <estherharriet2@gmail.com>
2022-07-05 09:16:56 -0700

    abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs-prints the absolute value of an integer
+ * @i: number targeted
+ * Return: Always 0 (success)
+ */
+int _abs(int i)
+{
+	if (i > 0)
+	return (i);
+	else if (i < 0)
+	return (-i);
+	else
+	return (0);
+}
