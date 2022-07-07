5866077a352c3fd7d4294793188caa3ac1fa6b74
devesty <estherharriet2@gmail.com>
2022-07-07 14:21:24 -0700

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,18 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i <= 9)
 	{
-	if (i == 2 && i == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(i);
+	if (i != 2 && i != 4)
+	_putchar(i + '0');
+	i++;
 	}
 	_putchar('\n')
-	}
 }
