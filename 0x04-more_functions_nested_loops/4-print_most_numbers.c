4c0c450c83f6b55c5b7c39589e6d145b400dc067
devesty <estherharriet2@gmail.com>
2022-07-07 05:52:19 -0700

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include
+
+/**
+ * print_most_numbers - function that prints numbers expect 2 and 4
+ *
+ * Return: Always 0 (success)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	if (i == 2 && i == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(i);
+	}
+	_putchar('\n')
+	}
+}
