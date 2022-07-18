f909a5675813d6550182f8abc41fba1947fc9438
devesty <estherharriet2@gmail.com>
2022-07-18 04:56:59 -0700

    Fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _mesmet - function that fills memory with a constant byte
+ * _memset - function that fills memory with a constant byte
  * @s: first parameter
  * @b: constant byte
  * @n: number of bytes
