af1e5cd0452e8af2a9468b0320659310633af4f4
devesty <estherharriet2@gmail.com>
2022-07-22 01:51:55 -0700

    Prints its name followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, followed by a new line
+ * @argc: number of arguments
+ * @argv: array of pointer to the string
+ * Return: return 0
+ */
+int main(int _attribute_((_unused_)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Agrc and Agrv
