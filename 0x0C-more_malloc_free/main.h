f639252b255cc2f1aa09ed66e67be88576209e2f
devesty <estherharriet2@gmail.com>
2022-07-28 15:11:24 -0700

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -11,6 +12,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+int find_len(char *str);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 
 #endif /* MAIN_H */
