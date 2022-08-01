829988a6ed48564de5bb1b00b019b38625b51299
devesty <estherharriet2@gmail.com>
2022-08-01 04:59:00 -0700

    Creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -62,7 +62,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	free(doggo);
 	return (NULL);
 	}
-	oggo->name = _strcopy(doggo->name, name);
+	doggo->name = _strcopy(doggo->name, name);
 	doggo->age = age;
 	doggo->owner = _strcopy(doggo->owner, owner);
 	return (doggo);
