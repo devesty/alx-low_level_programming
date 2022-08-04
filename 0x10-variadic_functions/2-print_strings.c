1835252caf8fa177600f1da0099786e34c3de9ca
devesty <estherharriet2@gmail.com>
2022-08-04 12:17:03 -0700

    Print a string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,24 +7,22 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	register unsigned int i;
-	va_list arguments;
+	va_list li;
+	unsigned int i;
 	char *str;
 
-	va_start(arguments, n);
+	va_start(li, n);
 
 	for (i = 0; i < n; i++)
 	{
-	str = va_arg(arguments, char *);
-	if (!str)
-	str = "(nil)";
-	if (!separator)
-	printf("%s", str);
-	else if (seperator && !i)
-	printf("%s", str)
+	str = va_arg(li, char *);
+	if (str == NULL)
+	printf("(nil)");
 	else
-	printf("%s%s", separator, str);
+	printf("%s", str);
+	if (i != (n - 1) && separator != NULL)
+	printf("%s", separator);
 	}
 	printf("\n");
-	va_end(arguments);
+	va_end(li);
 }
